18ad363ae4f9815b70c0126ccc45b30372ca1de0
s_zervos <s_zervos@wargaming.net>
2024-02-21 13:59:00 +0200

    Restructuring Bitstream lib, introducing default constructor, removing obsolete comments

--- Bitstream.cpp
+++ Bitstream.cpp
@@ -3,11 +3,16 @@
 
 #include "Bitstream.h"
 
-void Bitstream::PrintBinary()
+void Bitstream::AddByte(const unsigned char& InBit)
+{
+    bitstream_.emplace_back(InBit);
+}
+
+void Bitstream::PrintBinary() const
 {
     short unsigned BitCounter{ 0 };
-    std::cout << "Bit0\tBit1\tBit2\tBit3\tBit4\tBit5\tBit6\tBit7\n";
-    for (uint8_t Byte : _bitstream)
+    std::cout << "Byte0\tByte1\tByte2\tByte3\tByte4\tByte5\tByte6\tByte7\n";
+    for (const uint8_t Byte : bitstream_)
     {
         std::cout << std::hex << static_cast<int>(Byte) << "\t\t";
         if (!(BitCounter >= 7 ? BitCounter = 0 : ++BitCounter))
--- Bitstream.h
+++ Bitstream.h
@@ -6,16 +6,8 @@
 #include <vector>
 #include <iostream>
 
-/** @brief Converts data to binary data in the form of uint8_t. */
 class Bitstream
 {
-    /** @brief Contains the Binary Data. */
-    std::vector<uint8_t> _bitstream;
-
-    /** @brief The size of the Bitstream object. Equal to data() function of 
-     * std::vector. */
-    int _sizeOfBitstream = 0;
-
 public:
 
     /** @brief Construct a new Bitstream object by converting the given Array to Binary
@@ -25,8 +17,9 @@ public:
 
     Bitstream() = default;
     
-    std::vector<uint8_t> GetBitstream() const {return _bitstream;}
-
+    template<typename T>
+    void AddData(T InData);
+    
     /**
      * @brief Converts and Emplaces the Binary Data of the Bitstream to a std::vector 
      * of type T.
@@ -37,17 +30,20 @@ public:
     template<typename T>
     void Data2Obj(std::vector<T>& OutVectorOfObjs);
 
-    /**
-     * @brief Gets the SizeofBitstream object.
-     * 
-     * @return The number of bytes in the bitstream.
-     */
-    int GetSizeOfBitstream()const  {return _sizeOfBitstream;}
+    void AddByte(const unsigned char& InBit);
     
-    void PrintBinary();
+    /******** GETTERS ********/
 
-    template<typename T>
-    void AddData(T InData);
+    std::vector<uint8_t> GetBitstream() const {return bitstream_;}
+
+    /******** COSMETIC ********/
+    
+    void PrintBinary() const;
+
+private:
+    
+    /** @brief Contains the Binary Data. */
+    std::vector<uint8_t> bitstream_;
 };
 
 template<std::size_t N>
@@ -58,8 +54,7 @@ Bitstream::Bitstream(const double (&Obj2Data)[N])
         const uint8_t* Data = (uint8_t *)&Num; // Conversion of double to uint8_t
         for (size_t Datum = 0; Datum < sizeof(Num); ++Datum) // Since the converted data is an array we iterate through it
         {
-            _bitstream.emplace_back(Data[Datum]);
-            ++_sizeOfBitstream;
+            bitstream_.emplace_back(Data[Datum]);
         }
     }
 }
@@ -67,8 +62,8 @@ Bitstream::Bitstream(const double (&Obj2Data)[N])
 template<typename T>
 void Bitstream::Data2Obj(std::vector<T>& OutVectorOfObjs)
 {
-    uint8_t *Data = _bitstream.data();
-    for (size_t Offset = 0; Offset < _sizeOfBitstream; Offset += sizeof(T))
+    uint8_t *Data = bitstream_.data();
+    for (size_t Offset = 0; Offset < bitstream_.size(); Offset += sizeof(T))
     {
         T Obj;
         memcpy(&Obj, Data + Offset, sizeof(T));
@@ -82,7 +77,6 @@ void Bitstream::AddData(T InData)
     const uint8_t* Data = (uint8_t*)&InData; // Conversion of double to uint8_t
     for (size_t Datum = 0; Datum < sizeof(InData); ++Datum) // Since the converted data is an array we iterate through it
     {
-        _bitstream.emplace_back(Data[Datum]);
-        ++_sizeOfBitstream;
+        bitstream_.emplace_back(Data[Datum]);
     }
 }
